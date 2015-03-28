8e82c828b7d9a01cb157eaff5d059d5897f9b860
piorrro33 <piorrro33@outlook.com>
2015-03-28 01:08:10 +0100

    First commit.

---
+++ MegaManXComponent.cs
@@ -0,0 +1,20 @@
+﻿using LiveSplit.ASL;
+using LiveSplit.Model;
+using LiveSplit.Options;
+using LiveSplit.TimeFormatters;
+using LiveSplit.UI.Components;
+using LiveSplit.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveSplit.MegaManX
+{
+    class MegaManXComponent : LogicComponent
+    {
+        public MegaManXSettings Settings { get; set; }
+
+    }
+}
